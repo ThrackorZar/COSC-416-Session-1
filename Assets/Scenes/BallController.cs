7f07e21231c7f23e59f7225a7a6369366f252153
ThrackorZar <78045041+ThrackorZar@users.noreply.github.com>
2025-01-17 13:10:47 -0800

    added 3d rigidbody movement to ball

--- Assets/Scenes/BallController.cs
+++ Assets/Scenes/BallController.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class BallController : MonoBehaviour
 {
+    public Rigidbody sphereRigidbody;
+    public float ballSpeed = 2f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +15,31 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Update()
     {
         Debug.Log("Calling the Update method");
+        Vector2 inputVector = Vector2.zero; 
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector += Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector += Vector2.right;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;
+        }
+
+        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+
+        Debug.Log("Resultant Vector: " + inputVector);
+        Debug.Log("Resultant 3D Vector: " + inputXZPlane);
     }
 }
