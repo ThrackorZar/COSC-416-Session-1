335df64880a787b7ad7d69a5a1891c5301654a4a
ThrackorZar <78045041+ThrackorZar@users.noreply.github.com>
2025-01-17 13:47:07 -0800

    added ball colour, added jumping fucntionality

--- Assets/Scenes/BallController.cs
+++ Assets/Scenes/BallController.cs
@@ -4,10 +4,29 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private Rigidbody sphereRigidbody;
     [SerializeField] private float ballSpeed = 2f;
+    [SerializeField] private float jumpForce = 5f;
+    private bool isGrounded = true;
 
     public void MoveBall(Vector2 input)
     {
         Vector3 inputXZPlane = new Vector3(input.x, 0, input.y);
         sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            sphereRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
